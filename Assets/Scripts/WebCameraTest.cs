40f0c359153f27ae06313951802a139292e36e22
Suginuma Kyohei <suginuma.kyohei@sdtech.co.jp>
2021-02-01 17:09:55 +0900

    first commit

---
+++ Assets/Scripts/WebCameraTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCameraTest : MonoBehaviour {
+
+    public RawImage rawImage;
+
+    WebCamTexture webCamTexture;
+    WebCamDevice[] webCamDevice; /* 追加１ */
+    int selectCamera = 0; /* 追加 ２*/
+
+    void Start () {
+        webCamTexture = new WebCamTexture();
+        webCamDevice = WebCamTexture.devices; /* 追加３ */
+        rawImage.texture = webCamTexture;
+        webCamTexture.Play();
+	}
+
+    public void ChangeCamera() /* 追加４ */
+    {
+        int cameras = webCamDevice.Length; //カメラの個数
+        if ( cameras < 1) return; // カメラが1台しかなかったら実行せず終了
+
+        selectCamera++;
+        if (selectCamera >= cameras) selectCamera = 0;
+
+        webCamTexture.Stop(); // カメラを停止
+        webCamTexture = new WebCamTexture(webCamDevice[selectCamera].name); //カメラを変更
+        rawImage.texture = webCamTexture; 
+        webCamTexture.Play(); // 別カメラを開始
+    }
+}
